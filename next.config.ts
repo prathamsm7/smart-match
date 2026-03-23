7477689bc3ae1bcbd9ecb465688d5ec1c76e30c5
prathamsm7 <mungekarp8@gmail.com>
2026-03-23 17:42:12 +0530

    chore: Add `pdfjs-dist` to Next.js server external packages.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverExternalPackages: ['pdf-parse', '@qdrant/js-client-rest', 'redis', '@openai/agents', '@ai-sdk/google', 'ai', 'openai'],
+  serverExternalPackages: ['pdf-parse', 'pdfjs-dist', '@qdrant/js-client-rest', 'redis', '@openai/agents', '@ai-sdk/google', 'ai', 'openai'],
   eslint: {
     ignoreDuringBuilds: true,
   },
